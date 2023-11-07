fd1c5213beb088b8532df737daeef2c0dfed5c3b
Giuseppe Coviello <gcov@pm.me>
2023-11-06 22:42:03 -0500

    Removed include/dataType.h

--- include/kalmanFilter.h
+++ include/kalmanFilter.h
@@ -1,6 +1,17 @@
 #pragma once
 
-#include "dataType.h"
+#include <Eigen/Core>
+#include <Eigen/Dense>
+typedef Eigen::Matrix<float, 1, 4, Eigen::RowMajor> DETECTBOX;
+
+
+//Kalmanfilter
+typedef Eigen::Matrix<float, 1, 8, Eigen::RowMajor> KAL_MEAN;
+typedef Eigen::Matrix<float, 8, 8, Eigen::RowMajor> KAL_COVA;
+typedef Eigen::Matrix<float, 1, 4, Eigen::RowMajor> KAL_HMEAN;
+typedef Eigen::Matrix<float, 4, 4, Eigen::RowMajor> KAL_HCOVA;
+using KAL_DATA = std::pair<KAL_MEAN, KAL_COVA>;
+using KAL_HDATA = std::pair<KAL_HMEAN, KAL_HCOVA>;
 
 namespace byte_kalman
 {
